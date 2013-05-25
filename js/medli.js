3c95ecb78309440973657d48bb2b848de2cf5d8e
jaxbot <jaxbot@gmail.com>
2013-05-24 21:32:25 -0400

    Initial playing around

---
+++ js/medli.js
@@ -0,0 +1,63 @@
+/*
+ * Medli.js
+ * Main entry point for the board system 
+ */
+
+var _g = function(id) { return document.getElementById(id); }
+
+var pinItems = [];
+var categories = [];
+
+function renderPinItem(item) {
+	var e = document.createElement("div");
+	e.className = "pinitem";
+	e.setAttribute("data-category", item.category);
+	e.innerHTML = item.content;
+	_g("pincontainer").appendChild(e);
+
+	pinItems.push(item);
+	if (categories.indexOf(item.category) === -1) {
+		categories.push(item.category);
+		_g("linkscontainer").innerHTML += " <a href='#" + item.category + "'>" + item.category + "</a>";
+	}
+}
+
+function renderBoard(filter) {
+	if (typeof(filter) === "undefined")
+		var filter = "";
+
+	var x = 0;
+	var y = [];
+	var width = 300;
+	var maxwidth = getWindowWidth();
+	var padding = 40;
+
+	var children = _g("pincontainer").childNodes;
+	for (var i = 0; i < children.length; i++) {
+		if (filter && filter.split(",").indexOf(children[i].getAttribute("data-category")) === -1)
+		{
+			children[i].style.display = "none";
+			continue;
+		}
+		children[i].style.display = "block";
+
+		if (!y[x]) y[x] = 0;
+
+		children[i].style.left = (x * (width + padding)) + "px";
+		children[i].style.top = y[x] + "px";
+		y[x] += children[i].clientHeight + padding; 
+		x++;
+		if ((x * (width + padding)) >= maxwidth) {
+			x = 0;
+		}
+	}
+}
+
+function getWindowWidth() {
+	return window.innerWidth || document.documentElement.clientWidth;
+}
+
+window.addEventListener("hashchange", function() {
+	renderBoard(location.hash.substring(1));
+}, true);
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+Medli.js
+
