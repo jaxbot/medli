20656535747c554500a5d52c1f5c3c72419bbe68
jaxbot <jaxbot@gmail.com>
2013-07-01 19:47:58 -0400

    Design tweaks

--- include/header.php
+++ include/header.php
@@ -27,25 +27,25 @@ if ($PRODUCTION) {
 <script type="text/javascript">
 var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-10561014-2']);_gaq.push(['_trackPageview']);
 (function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 </script>
 <script src='//platform.twitter.com/widgets.js' async='true'></script>
 </head>
 <body>
 <div id='header'>
-	<a href='./'><h1>Jaxbot</h1></a>
-	<div id='info'>
-<?php if ($postPage) { ?>
-		<a href="https://twitter.com/share" class="twitter-share-button" data-via="Jaxbot" data-size="large" data-count="none">Tweet</a>
-<?php } ?>
-		<a href="https://twitter.com/jaxbot" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @jaxbot</a>
-	</div>
-	<div id='linkscontainer'>
-		<a href='./'>home</a>
-		<a href='http://twitter.com/jaxbot'>twitter</a>
-		<a href='http://flickr.com/jaxbot'>flickr</a>
-	</div>
+    <a href='./'><h1 title='Developer. 18. Node, HTML5, CSS3. Friendly; talk to me'>Jaxbot</h1></a>
+    <div id='info'>
+        <?php if ($postPage) { ?>
+        <a href="https://twitter.com/share" class="twitter-share-button" data-via="Jaxbot" data-size="large" data-count="none">Tweet</a>
+        <?php } ?>
+        <a href="https://twitter.com/jaxbot" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @jaxbot</a>
+    </div>
+    <div id='linkscontainer'>
+        <a href='./'>home</a>
+        <a href='http://twitter.com/jaxbot'>twitter</a>
+        <a href='mailto:jaxbot@gmail.com'>contact</a>
+    </div>
 </div>
